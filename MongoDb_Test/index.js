4f0ede87cf9918cb506458c337d60d26df5f55ba
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2024-08-09 08:42:03 +0530

    Added comments

--- MongoDb_Test/index.js
+++ MongoDb_Test/index.js
@@ -21,18 +21,23 @@ const User = mongoose.model("Users12", {
 app.post("/signup", async function (req, res) {
         const { name, username, password } = req.body;
 
+        // if user exists then, find one from the User collection by comparing with the username
         const userExists = await User.findOne({ username: username });
         if (userExists) {
                 return res.status(400).send("User already exists");
         } else {
+                // user doesn't exist, create a new user by the User model.
                 const user = new User({
                         name: name,
                         username: username,
                         password: password,
                 });
 
+                // save the user
                 user.save();
+
                 console.log(user)
+                // show the output as status 200, with a clear message.
                 res.status(200).send("User created successfully");
         }
 });
@@ -40,11 +45,15 @@ app.post("/signup", async function (req, res) {
 // get method for the user to get back with the username and password
 app.get("/users", async function (req, res){
         try{
+                // pass the username in the headers
                 const username = req.headers.username;
+
+                // find one user with the username-header
                 const user = await User.findOne({username: username})
                 console.log(user);
+                // return or show the user details
                 res.json({
-                        userDetails : user
+                        user_details : user
                 })
         } catch{
                 res.status(411).send("Something bad happened.");

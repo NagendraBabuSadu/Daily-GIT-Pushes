f05be0c99efbedde49c96dff8dea914200942f03
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2024-08-17 10:59:28 +0530

    Added /interest get http request for the Interest calculation

--- DOM Intro/index.js
+++ DOM Intro/index.js
@@ -12,4 +12,20 @@ app.get("/sum", async function (req, res) {
         res.status(200).send(`sum is ${sum}`);
 })
 
+
+app.get('/interest', async function (req, res) {
+        const principal = req.query.principal;
+        const rate = req.query.rate;
+        const time = req.query.time;
+
+        let interestRate = parseFloat(rate * time);
+        let totalAmount =parseFloat(principal) + parseFloat(interestRate);
+        console.log(time);
+        res.status(200).json({
+                "Interest Rate" : interestRate,
+                "Total" : totalAmount
+        })
+})
+
+
 app.listen(3000);

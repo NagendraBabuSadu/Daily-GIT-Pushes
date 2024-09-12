919ee1d3058febea705ef1c4d104047c346f91a3
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2024-09-12 09:20:39 +0530

    Added newTodo functionality

--- React Topics/Todo-App/src/App.jsx
+++ React Topics/Todo-App/src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
-
+import { useState } from 'react';
 
 
 function App() {
+	const [newTodoTitle, setNewTodoTitle] = useState("");
+	const [newTodoDescription, setNewTodoDescription] = useState("");
+
 	const [todos, setTodos] = useState([
 		{
 			id: 1,
@@ -18,17 +20,20 @@ function App() {
 	function addTodo() {
 		let newtodo = {
 			id: todos.length + 1,
-			title: "New Title",
-			description: "New Desc"
+			title: newTodoTitle,
+			description: newTodoDescription
 		}
 		setTodos([newtodo, ...todos])
+
+		setNewTodoTitle("");
+		setNewTodoDescription("");
+
 	}
 
 	function updateTodo(id, updatedTodo) {
 		const updatedTodos = todos.map((todo) => {
-
-			if(todo.id === id) {
-				return {...todo, ...updatedTodo}
+			if (todo.id === id) {
+				return { ...updatedTodo }
 			}
 			return todo;
 		});
@@ -37,13 +42,33 @@ function App() {
 
 	return (
 		<div>
+			{/* Add Todo Inputs */}
+			<label>
+				Title:
+				<input
+					type="text"
+					placeholder="Enter title"
+					value={newTodoTitle}
+					onChange={(e) => setNewTodoTitle(e.target.value)}
+				/>
+			</label>
+			<label htmlFor="">
+				Description:
+				<input
+					type="text"
+					placeholder="Enter description"
+					value={newTodoDescription}// Controlled input for description
+					onChange={(e) => setNewTodoDescription(e.target.value)}
+				/>
+			</label>
+
+
 			<button onClick={addTodo}>Add Todo</button>
 			{todos.map((todo) => {
 				return (
 					<Todo
 						key={todo.id} id={todo.id} title={todo.title} description={todo.description} updateTodo={updateTodo}
 					/>
-
 				)
 			})}
 		</div>
@@ -51,39 +76,68 @@ function App() {
 }
 
 function Todo(props) {
-	const [isEditing, setIsEditing] = useState(false);
+	const [isEditingTitle, setIsEditingTitle] = useState(false);
+	const [isEditingDescription, setIsEditingDescription] = useState(false);
 	const [title, setTitle] = useState(props.title);
 	const [description, setDescription] = useState(props.description);
 
 
-	function handleSave() {
-		setIsEditing(false);
-		props.updateTodo(props.id, { id: props.id, title,  description });
+
+	function titleMouseOver(e) {
+		setIsEditingTitle(true);
+		setIsEditingDescription(false);
+		setTitle(e.target.value);
+	}
+
+	function descriptionMouseOver(e) {
+		setIsEditingTitle(false);
+		setIsEditingDescription(true);
+		setDescription(e.target.value);
 	}
 
 
-	function editTodo() {
-		setIsEditing(true);
+	function saveTodo() {
+		setIsEditingTitle(false);
+		setIsEditingDescription(false);
+		props.updateTodo(props.id, {
+			id: props.id,
+			title,
+			description
+		})
+	}
+
+	function Button({ onClick, label }) {
+		return (
+			<button onClick={onClick}>{label}</button>
+		)
 	}
 
 	return (
 		<div>
-			{isEditing ? (
+			{isEditingTitle ? (
 				<div>
 					<input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-					<input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
-					<button onClick={handleSave}>Save</button>
+					<Button onClick={saveTodo} label="Save" />
 				</div>
 			) : (
-				<div>
-					<h3>{props.title}</h3> 	
-					<h3>{props.description}</h3> 	
-					<button onClick={editTodo}>Edit</button>
-				</div>
+				<h3 onClick={titleMouseOver}>{props.title}</h3>
 			)}
+			{isEditingDescription ? (
+				<div>
+					<input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
+					<Button onClick={saveTodo} label="Save" />
+				</div>
+			) : (
+				<h3 onClick={descriptionMouseOver}>{props.description}</h3>
+			)}
+
+
 		</div>
 	)
 
 }
 
 export default App
+
+
+

23ea25f455c65db69d9bbd4f0cfcde50390d7cb4
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2024-09-25 22:50:19 +0530

    style is added to todos.jsx file

--- FullStack-Todo-App/frontend/src/components/todos.jsx
+++ FullStack-Todo-App/frontend/src/components/todos.jsx
@@ -3,23 +3,28 @@ import { useState } from "react"
 export function Todos({ id, todos, setTodos }) {
 
 
-    function toggleTodo(todo) {
-        const updatedTodos = todos.map((todoItem) => todoItem.id === todo.id ? { ...todoItem, completed: !todoItem.completed } : todoItem)
 
-        setTodos(updatedTodos);  
+    function toggleTodo(todo) {
+        setTodos(todos => todos.map((todoItem) => todoItem._id === todo._id ? { ...todoItem, completed: !todoItem.completed } : todoItem));
+
     }
 
+
+
     return (
-        <div>
+        <div style={{
+            overflowY: "auto"
+        }}>
             {todos && todos.length > 0 ? (
-                todos.map(function (todo, index) {
-                    console.log(todo) // <-- DO THIS    
+                todos.map(function (todo, index) {    
                     return (
-                        <div key={todo.id}>
-                            <h3>{id + index}</h3>
-                            <h3>{todo.title}</h3>
-                            <h3>{todo.description}</h3>
-                            <button id={id + index} key={todo.id} onClick={() => toggleTodo(todo)}>{todo.completed === false ? "Mark as Done" : "Completed"}</button>
+                        <div style={{
+                            display: "flex"
+                        }} key={todo._id}>
+                            <h3 style={{margin: "2px"}}>{id + index}</h3> 
+                            <h3 style={{margin: "2px"}}>{todo.title}</h3>
+                            <h3 style={{margin: "2px"}}>{todo.description}</h3>
+                            <button style={{margin: "2px"}} id={todo._id + index} key={todo._id} onClick={() => toggleTodo(todo)}>{todo.completed === false ? "Mark as Done" : "Completed"}</button>
                         </div>
                     )
                 }

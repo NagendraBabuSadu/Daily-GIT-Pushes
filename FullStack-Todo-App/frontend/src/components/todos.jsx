f782cac13afb0051f948a9c8a830a148baede19e
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2024-09-29 06:16:38 +0530

    Removed id and key for button

--- FullStack-Todo-App/frontend/src/components/todos.jsx
+++ FullStack-Todo-App/frontend/src/components/todos.jsx
@@ -24,7 +24,7 @@ export function Todos({ id, todos, setTodos }) {
                             <h3 style={{margin: "2px"}}>{id + index}</h3> 
                             <h3 style={{margin: "2px"}}>{todo.title}</h3>
                             <h3 style={{margin: "2px"}}>{todo.description}</h3>
-                            <button style={{margin: "2px"}} id={todo._id + index} key={todo._id} onClick={() => toggleTodo(todo)}>{todo.completed === false ? "Mark as Done" : "Completed"}</button>
+                            <button style={{margin: "2px"}} onClick={() => toggleTodo(todo)}>{todo.completed === false ? "Mark as Done" : "Completed"}</button>
                         </div>
                     )
                 }

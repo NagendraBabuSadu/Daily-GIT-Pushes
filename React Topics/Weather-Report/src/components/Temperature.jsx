e7e06f6ed22010b3cac7bf394ceed7fb5d4b28fa
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2024-10-06 12:40:15 +0530

    Updated Temperature component

--- React Topics/Weather-Report/src/components/Temperature.jsx
+++ React Topics/Weather-Report/src/components/Temperature.jsx
@@ -1,55 +1,18 @@
-import { useState, useEffect } from "react";
-
-function Temperature() {
-  const [temp, setTemp] = useState(null);
-
-  useEffect(() => {
-    const fetchWeather = async () => {
-      const response = await fetch(
-        "https://api.open-meteo.com/v1/forecast?latitude=28.6139&longitude=77.2090&hourly=temperature_2m"
-      );
-      const data = await response.json();
-      const list = await data.hourly.temperature_2m;
-      console.log(list);
-
-      if (list.length === 0) {
-        console.log("No data available");
-        setTemp("No data available");
-        return;
-      }
-
-      let index = 0;
-      let newTemp = setTemp(list[index]);
-
-      const intervalId = setInterval(() => {
-        index++;
-        if (index < list.length) {
-          setTemp(list[index]);
-          console.log(list[index]);
-        } else {
-          clearInterval(intervalId);
-        }
-      }, 60 * 1000);
-    };
-    fetchWeather();
-  }, [setTemp]);
+function Temperature({ temp }) {
+  const tempStyle = {
+    fontSize: '3em',
+    fontweight: 'bold',
+    fontFamily: 'sans-serif',
+    color: temp === null ? 'red' : 'white'
+  };
 
   return (
-    <div
-      style={{
-        textAlign: "center",
-        fontSize: "1rem",
-        textShadow: "rgb(12 123 141) 3px 1px 2px",
-      }}
-    >
-      <h2>Temperature</h2> <span></span>
-      {temp === null ? (
-        <h2>Loading...</h2>
-      ) : (
-        <h2 style={{ fontSize: "2.8rem" }}>{temp}°C</h2>
-      )}
+    <div>
+      <h2>Temperature</h2>
+      <p style={tempStyle}>
+        {temp === null ? "Loading..." : `${temp} °C`}
+      </p>
     </div>
   );
 }
-
 export default Temperature;

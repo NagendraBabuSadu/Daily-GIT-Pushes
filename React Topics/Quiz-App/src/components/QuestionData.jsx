6af6376384bf261b7734222ffa2c4b98214e42bc
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2024-10-08 08:26:12 +0530

    Created QuestionData component

---
+++ React Topics/Quiz-App/src/components/QuestionData.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect } from "react";
+
+function QuestionData() {
+  const [questions, setQuestions] = React.useState([]);
+  const [answers, setAnswers] = React.useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const storedData = localStorage.getItem("questions");
+      if (storedData) {
+        const parsedData = JSON.parse(storedData);
+        setQuestions(parsedData.results);
+        console.log(parsedData.results);
+        const correctAnswers = parsedData.results.map(
+          (item) => item.correct_answer
+        );
+        console.log(correctAnswers);
+        const incorrectAnswers = parsedData.results.map(
+          (item) => item.incorrect_answers
+        );
+        console.log(incorrectAnswers);
+        const allAnswers = parsedData.results.map((item) => [
+          item.correct_answer,
+          ...item.incorrect_answers,
+        ]);
+        setAnswers(allAnswers);
+        console.log(allAnswers);
+        return;
+      } else {
+        const response = await fetch(
+          "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
+        );
+        const data = await response.json();
+        localStorage.setItem("questions", data);
+        setQuestions(data);
+      }
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <form action="" method="get">
+        {questions.map((item, questionIndex) => {
+          return (
+            <div key={questionIndex}>
+              <h3>{questionIndex + 1} <span></span> {item.question}</h3>
+              <div>
+
+              <ul>
+                {answers[questionIndex].map((answer, answerIndex) => {
+                    return (
+                        <div>
+
+                    <li key={answerIndex} style={{ listStyleType: "none" }}>
+                      <input
+                        type="radio"
+                        name={`question${questionIndex}`}
+                        id={`answer${questionIndex}_${answerIndex}`}
+                        value={answer}
+                        />
+                      <label htmlFor={`answer${questionIndex}_${answerIndex}`}>{answer}</label>
+                    </li>
+                        </div>
+                  );
+                })}
+              </ul>
+                </div>
+            </div>
+          );
+        })}
+      </form>
+        <button type="submit">Submit</button>
+    </div>
+  );
+}
+
+export default QuestionData;

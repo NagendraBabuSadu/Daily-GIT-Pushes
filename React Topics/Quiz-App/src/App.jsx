19ef4ee16e437a41e6b56bbb19e94ddbec1702a0
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2024-10-08 08:25:57 +0530

    Updated App component

---
+++ React Topics/Quiz-App/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import QuestionData from './components/QuestionData';  
+import './App.css';
+function App(){
+
+
+  return(
+    <div >
+      <h1>Quiz App</h1>
+      <QuestionData />
+    </div>
+  )
+}
+
+export default App;

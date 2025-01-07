b4a098ad80bf2b7b1f9fbd767bd3052b33d2fb83
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2025-01-07 09:23:06 +0530

    Added Symbol whenever user clicks on, to show it and also Styling.

--- React Topics/Calc/src/App.jsx
+++ React Topics/Calc/src/App.jsx
@@ -7,24 +7,38 @@ function App() {
 	const [num1, setNum1] = useState();
 	const [num2, setNum2] = useState();
 	const [result, setResult] = useState(0);
+	const [symbol, setSymbol] = useState("");
 
 	const handleAddition = () => {
 		setResult(Number(num1) + Number(num2));
+		setSymbol("+")
 	}
 	const handleSubtraction = () => {
 		setResult(Number(num1) - Number(num2));
+		setSymbol("-")
 	}
 	const handleMultiplication = () => {
 		setResult(Number(num1) * Number(num2));
+		setSymbol("*")
 	}
 	const handleDivision = () => {
 		setResult(Number(num1) / Number(num2));
+		setSymbol("/")
 	}
 	return (
 		<div>
+			<div>
+				<h3 style={{color:"darkred"}}>Basic Calculator</h3>
+			</div>
+		<div style={{border: "2px solid grey", padding: "18px", borderRadius: "5px"}}>
 
 			<input type="number" onChange={(e) => setNum1(e.target.value)} />
-			<br /><br />
+			<br />
+				
+
+			<p style={{position: "fixed", transform: "scale(1.7)"}}>{symbol}</p>
+			
+
 			<input type="number" onChange={(e) => setNum2(e.target.value)} />
 
 			<br /><br />
@@ -39,12 +53,13 @@ function App() {
 				<button onClick={handleMultiplication}>*</button>
 				<span> </span>
 
-				<button onClick={handleDivision}>*</button>
+				<button onClick={handleDivision}>/</button>
 			</div>
 
-			<h4>Result is {result}</h4>
+			<h4>Result is <span style={{color: "darkviolet"}}> {result.toFixed(2)}</span></h4>
 
 
+		</div>
 		</div>
 	)
 
